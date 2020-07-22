9a486c9fb178cc19c17bb49887066ae17fc7b76a
unknown <moniquejroliveira@gmail.com>
2020-07-21 22:36:25 -0300

    inserindo dados

--- data.sql
+++ data.sql
@@ -1,10 +1,1 @@
-use db_pessoas;
-create table funcionarios(
-id bigint(5) auto_increment,
-nome varchar(20) not null,
-cpf varchar(11),
-cel bigint(9),
-endereco varchar(100),
-email varchar(127),
-primary key (id)
-);
+select * From funcionarios
