60ed9c96de2870559d8b7f9af22731bb5c60f269
unknown <moniquejroliveira@gmail.com>
2020-07-23 12:16:27 -0300

    create table, join, inner, where, select

--- aula06.sql
+++ aula06.sql
@@ -89,3 +89,31 @@ insert into tb_produtos (nome, preco, marca_id) values ("Sapato", "52.34", 6);
 insert into tb_produtos (nome, preco, marca_id) values ("Sapato", "23.88", 3);
 
 select * from tb_produtos
+
+select * from tb_produtos
+inner join tb_marcas on tb_marcas.id = tb_produtos.marca_id
+
+select tb_produtos.nome, tb_produtos.preco, tb_marcas.nome
+	from tb_produtos
+inner join tb_marcas on tb_marcas.id = tb_produtos.marca_id
+where tb_marcas.nome like "Zara"
+
+select tb_produtos.nome, tb_produtos.preco, tb_marcas.nome
+	from tb_produtos
+inner join tb_marcas on tb_marcas.id = tb_produtos.marca_id
+where tb_marcas.nome like "Zara"
+	and tb_produtos.preco < 100.00
+    and tb_produtos.nome = "Meias"
+
+select tb_produtos.nome, tb_produtos.preco, tb_marcas.nome
+	from tb_produtos
+inner join tb_marcas on tb_marcas.id = tb_produtos.marca_id
+where tb_marcas.nome = "Meias"
+	or tb_produtos.nome = "Tênis"
+
+select *
+	from tb_produtos
+left join tb_marcas on tb_marcas.id = tb_produtos.marca_id
+select *
+	from tb_produtos
+right join tb_marcas on tb_marcas.id = tb_produtos.marca_id
