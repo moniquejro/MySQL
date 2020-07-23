31d8275953364d17193eafe3a6f7dc1f19d93225
unknown <moniquejroliveira@gmail.com>
2020-07-22 22:18:51 -0300

    first table

---
+++ rh22tab1.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_cargo22(nvl, setor) VALUES("Estagiario", "TI");
+INSERT INTO tb_cargo22(nvl, setor) VALUES("Junior", "TI");
+INSERT INTO tb_cargo22(nvl, setor) VALUES("Pleno", "TI");
+INSERT INTO tb_cargo22(nvl, setor) VALUES("Senior", "TI");
+INSERT INTO tb_cargo22(nvl, setor) VALUES("Especialista", "TI");
