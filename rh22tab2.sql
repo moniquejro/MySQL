f52b7bce8a0dbe8c23e5b322fa304500273b2128
unknown <moniquejroliveira@gmail.com>
2020-07-22 22:20:02 -0300

    second table

---
+++ rh22tab2.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_funcionario22(nome, email, endereco, cel, salario, id_cargo) VALUES("Jessica", "fulana@generation.com", "Av Rosa 10", "00000000", 2300, 3);
+INSERT INTO tb_funcionario22(nome, email, endereco, cel, salario, id_cargo) VALUES("Viviane", "fulana@generation.com", "Av Margarida 20", "11111111", 2000, 4);
+INSERT INTO tb_funcionario22(nome, email, endereco, cel, salario, id_cargo) VALUES("Bruna", "fulana@generation.com", "Av Comigo Ninguem Pode 30", "22222222", 890, 1);
+INSERT INTO tb_funcionario22(nome, email, endereco, cel, salario, id_cargo) VALUES("Monique", "fulana@generation.com", "Av Rosas 40", "33333333", 1000, 7);
+INSERT INTO tb_funcionario22(nome, email, endereco, cel, salario, id_cargo) VALUES("Agatha", "fulana@generation.com", "Av Samambaia 50", "99999999", 4100, 10);
